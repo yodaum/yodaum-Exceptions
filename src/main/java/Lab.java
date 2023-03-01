671170cb23d9620b1021adfdabccc1ad552c4813
yodaum <yod.aum@gmail.com>
2023-03-01 00:19:10 +0000

    Complete Exception Test Lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,7 +20,8 @@ public class Lab {
      *
      * Check out this guide on throwing exceptions: https://rollbar.com/guides/java/how-to-throw-exceptions-in-java/#
      */
-    public void mustThrow() {
-
+    public void mustThrow() throws Exception{
+        String test = null;
+        test.toString();
     }
 }
